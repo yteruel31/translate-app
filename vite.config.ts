4174e23bcec0d8dfbdc1037f376c9409aa9c8dc3
Yoann TERUEL <yoann.teruel@gmail.com>
2024-02-03 15:11:35 +0100

    chore: configure type checking, linting and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "localhost",
+    open: true,
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint --ext js,ts,tsx src",
+      },
+    }),
+  ],
+});
